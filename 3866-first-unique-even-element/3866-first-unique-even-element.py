88d99fc2af15783e9582f6b3e9bdb058106c06c0
Shivam Kumar <shivamkumar@alumni.iitm.ac.in>
2026-08-15 15:40:06 +0530

    Time: 1 ms (56.48%), Space: 19.2 MB (91.36%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstUniqueEven(self, nums: list[int]) -> int:
+        dic={}
+        for i in nums:
+            if i not in dic:
+                dic[i]=1
+            else:
+                dic[i]+=1
+        for i in dic:
+            if dic[i]==1 and i%2==0:
+                return i
+        return -1
+  
