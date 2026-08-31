89f394d1dde5c36dbde06bdcf99a437b5f709393
Shivam Kumar <shivamkumar@alumni.iitm.ac.in>
2026-09-01 00:43:18 +0530

    Time: 3 ms (62.1%), Space: 19.2 MB (95.7%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        dic = {}
+        for ch in text:
+            dic[ch] = dic.get(ch, 0) + 1
+        b = dic.get("b", 0)
+        a = dic.get("a", 0)
+        l = dic.get("l", 0) // 2
+        o = dic.get("o", 0) // 2
+        n = dic.get("n", 0)
+        return min(b, a, l, o, n)
