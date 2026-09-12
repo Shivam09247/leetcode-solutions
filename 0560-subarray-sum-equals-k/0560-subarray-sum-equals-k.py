d81b86f0ce9b3a9e17da7969f3222ac8b1d11ac0
Shivam Kumar <shivamkumar@alumni.iitm.ac.in>
2026-09-12 23:25:56 +0530

    Time: 39 ms (27.59%), Space: 22.2 MB (21.57%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        dic = {0: 1}
+        prefix = 0
+        count = 0
+        for i in range(len(nums)):
+            prefix += nums[i]
+            if prefix - k in dic:
+                count += dic[prefix - k]
+            dic[prefix] = dic.get(prefix, 0) + 1
+        return count
+        
